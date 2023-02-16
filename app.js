f03b6b7d191e69d3013c1cd01aad778bf01a7088
Leechiayu <gary86442@gmail.com>
2023-02-16 19:46:07 +0800

    feat:add Creat function

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.static("public"));
 
 // MongoDB  setting
 const mongoose = require("mongoose");
+const restaurantDB = require("./models/restaurant");
+
 // use dotenv only in testing
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -26,9 +28,47 @@ db.on("error", () => {
 db.once("open", () => {
   console.log("DB connect!");
 });
+// body-paser
+app.use(express.urlencoded({ extended: true }));
+
 //handling route
 app.get("/", (req, res) => {
-  res.render("index");
+  restaurantDB
+    .find()
+    .lean()
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((error) => console.log(error));
+});
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
+  restaurantDB
+    .findById(id)
+    .lean()
+    .then((restaurant) => res.render("show", { restaurant }))
+    .catch((error) => console.log(error));
+});
+// hadling creat restaurant
+app.get("/restaurant/new", (req, res) => {
+  res.render("new");
+});
+app.post("/restaurants", (req, res) => {
+  const restaurant = req.body;
+  return restaurantDB
+    .create({
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description,
+    })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => console.log(error));
 });
 
 // listen server
